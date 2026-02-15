9232a83dcd70106d43c090d5574cca1bbf6ac986
contactme-soumya <contactme.soumya@gmail.com>
2026-02-15 08:13:57 +0530

    LangChain chat model use and embedding model use for finding similarity

---
+++ ChatModels/chatModel_hf_local.py
@@ -0,0 +1,28 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFacePipeline
+from dotenv import load_dotenv
+import os
+os.environ["HF_HOME"] = 'D:/HF_AI_Models'
+
+load_dotenv(override=True)
+
+# try:
+llm = HuggingFacePipeline.from_model_id(
+    model_id='TinyLlama/TinyLlama-1.1B-Chat-v1.0',
+    task="text-generation",
+    pipeline_kwargs={"temperature": 0.5,"max_new_tokens": 100}
+)
+model = ChatHuggingFace(llm=llm)
+result = model.invoke("What is the capital of India?")
+print(result.content)   
+# except Exception as e:
+#     print(f"Error: {e}")
+#     print("\nTrying alternative: Using a model that's more likely to work with Inference API...")
+#     llm = HuggingFacePipeline.from_model_id(
+#         model_id='meta-llama/Llama-3.2-1B-Instruct',
+#         task="text-generation",
+#         pipeline_kwargs={"temperature": 0.5,"max_new_tokens": 100}
+#     )
+#     model = ChatHuggingFace(llm=llm)
+#     result = model.invoke("What is the capital of India?")
+#     print(result.content)
+    
